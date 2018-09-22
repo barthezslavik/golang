dee02883aa555b25bdc97202137d7fe88b26767a
Loginov Vyacheslav <barthez.slavik@gmail.com>
2018-09-22 03:05:23 +0300

    Add domain

--- dive/main.go
+++ dive/main.go
@@ -2,24 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/barthezslavik/golang/dive/lib"
+	"github.com/barthezslavik/golang/dive/domain"
 )
 
-type Object struct {
-	Content struct {
-		Name string
-	}
-}
-
 func main() {
-	object := &Object{}
-	object.Content.Name = "Text"
-	fmt.Println(object)
 
-	lib.Add("dr", "Dart")
-	fmt.Println(lib.Get("dr"))
-	languages := lib.GetAll()
-	for _, v := range languages {
-		fmt.Println(v)
-	}
 }
